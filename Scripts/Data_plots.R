65d8671683ae1cb34d53ab48c29b451809eee6a0
Mirai <49144878+TorAdagio@users.noreply.github.com>
2021-05-30 22:08:44 +0200

    Filtros creados

--- Scripts/Data_plots.R
+++ Scripts/Data_plots.R
@@ -1,2 +1,18 @@
 source("Scripts/Data_preparing.R")
 policia
+
+
+policia %>%
+  filter(grepl("furt", policia$tipus ) )
+
+
+
+policia %>%
+  filter(grepl("drogues", policia$tipus ) )
+
+
+policia %>%
+  filter(grepl("sex", policia$tipus ) )
+
+
+
