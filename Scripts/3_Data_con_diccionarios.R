d2a6e5e7ad1133925fcd6a7a1ee4370e21f55152
Mirai <49144878+TorAdagio@users.noreply.github.com>
2021-07-25 18:27:15 +0200

    Arreglo de formatos

--- Scripts/3_Data_con_diccionarios.R
+++ Scripts/3_Data_con_diccionarios.R
@@ -1,7 +1,6 @@
-source("Scripts/2_Diccionario_tipos_delitos.R")
-Encoding("UFT-8")
+source("Scripts/2_Diccionario_tipos_delitos.R", encoding = "UTF-8")
 
-## CategorÃ­as generales
+## Categorías generales
 
 policia<- policia %>%
   mutate(tipus = as.character(tipus) ) %>%
@@ -12,7 +11,7 @@ policia<- policia %>%
     tipus %in% transgresiones_altres ~ "Otros"))%>%
   as.data.frame(.)
 
-## SubcategorÃ­as
+## Subcategorías
 policia<- policia %>%
   mutate(tipus = as.character(tipus) ) %>%
   mutate( sub_tipus = case_when(
@@ -22,7 +21,7 @@ policia<- policia %>%
     tipus %in% transgresiones_medioambiente ~ "Medioambiente",
     tipus %in% transgresiones_consumidor ~ "Al consumidor",
     tipus %in% trangresiones_propiedad_privada ~ "Propiedad privada",
-    tipus %in% transgresiones_danos ~ "DaÃ±os",
+    tipus %in% transgresiones_danos ~ "Daños",
     tipus %in% trangresiones_homicidos ~ "Homicidios",
     tipus %in% transgresiones_familia ~"Familia",
     tipus %in% transgresiones_fisico ~ "Fisico",
@@ -36,7 +35,7 @@ policia<- policia %>%
     tipus %in% transgresiones_drogas ~ "Drogas",
     tipus %in% transgresiones_comportamientos_disidentes ~ "Comportamientos disidentes",
     tipus %in% trangresiones_seguridad_vial ~ "Seguridad vial",
-    tipus %in% transgresiones_salud_publica ~ "Salud pÃºblica",
+    tipus %in% transgresiones_salud_publica ~ "Salud pública",
     tipus %in% transgresiones_altres ~ "Otros"))%>%
   as.data.frame(.)
 
